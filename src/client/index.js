979a0926807095272988fc77618aa648ba585cc2
Mohamed Gad <mohamedgad149@gmail.com>
2021-11-27 18:23:32 +0200

    i have added loaders to convert sass files into css

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,9 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
+import "./style/reset.scss"
+import "./style/base.scss"
 
 console.log(checkForName);
 
-alert("I EXIST")
+// alert("I EXIST")
 console.log("CHANGE!!");
