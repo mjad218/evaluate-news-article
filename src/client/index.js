759bf892c611078892d7a782d663874963927a49
Mohamed Gad <mohamedgad149@gmail.com>
2021-11-27 18:47:07 +0200

    i added style

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,10 @@ import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 import "./style/reset.scss"
 import "./style/base.scss"
+import "./style/footer.scss"
+import "./style/form.scss"
+import "./style/header.scss"
+import "./style/main.scss"
 
 console.log(checkForName);
 
