60454bc73da20299d2b37a1841a4cccaf358eade
Mohamed Gad <mohamedgad149@gmail.com>
2021-11-28 15:15:47 +0200

    I finshed setting up the routing to get the data from the API

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,14 +2,17 @@ function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+    let formText = document.getElementById('url').value;
+    Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
+    fetch(`http://localhost:8081/meaning?url=${formText}`, {
+        // body: formText
+    })
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+        document.getElementById('results').innerHTML = `<pre> ${JSON.stringify(res) }</pre>`;
+        console.log(res);
     })
 }
 
--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,18 @@
-var path = require('path')
 const express = require('express')
-// const mockAPIResponse = require('./mockAPI.js')
+var FormData = require('form-data');
 
 const app = express()
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+const cors = require("cors");
+
+const corsOptions ={
+   origin:'*', 
+   credentials:true,
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) 
 
 app.use(express.static('dist'))
 
@@ -18,6 +28,28 @@ app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
 })
 
-app.get('/test', function (req, res) {
-    // res.send(mockAPIResponse)
+const getAPIData = async (url) => {
+
+    const formData = new FormData();
+    formData.append("key", "");
+    formData.append("url",url);
+    formData.append("lang", "en"); 
+
+    let data = await fetch("https://api.meaningcloud.com/sentiment-2.1" , {
+        method: 'POST',
+        body: formData,
+        redirect: 'follow',
+        headers:{
+            'Content-Type': "multipart/form-data"
+        }
+    });
+    data = await data.json();
+    return data; 
+} 
+app.get('/meaning', async (req, res) => {
+    console.log(req.query.url);
+    const data = await getAPIData(req.query.url); 
+    console.log(data);
+    console.log(typeof data);
+    res.send(JSON.stringify(data))
 })
